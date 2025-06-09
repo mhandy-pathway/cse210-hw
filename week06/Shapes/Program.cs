e566ce6e7bed9dd8a237bd9135c826e3d83f0352
Matt Handy <matt@handygolf.com>
2025-06-08 21:36:31 -0300

    Completed W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Yellow", 3, 7);
+        Circle circle = new Circle("Blue", 5);
+
+        List<Shape> shapeList = new List<Shape>([square, rectangle, circle]);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"{shape.GetType().Name} - Color: {shape.GetColor()} - Area: {shape.GetArea()}");
+        }
     }
 }
